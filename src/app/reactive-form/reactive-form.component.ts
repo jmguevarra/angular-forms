621bffdcb4b504cb82eb903978a46a0b67940a81
jmguevarra <jaimeguevarra22@gmail.com>
2022-05-02 16:07:06 +1000

    Nested Angular FormGroup

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -19,8 +19,10 @@ export class ReactiveFormComponent implements OnInit {
      * - Async
      */
     this.signupForm = new FormGroup({
-      'username'  :   new FormControl(null, Validators.required),
-      'email'     :   new FormControl(null, [Validators.required, Validators.email]),
+      'userData'  :   new FormGroup({
+        'username'  :   new FormControl(null, Validators.required),
+        'email'     :   new FormControl(null, [Validators.required, Validators.email]),
+      }),
       'gender'    :   new FormControl('Male') 
     });
   }
