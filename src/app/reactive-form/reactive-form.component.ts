a3c09b13c7140140c784b82b02d8f355daa86f51
jmguevarra <jaimeguevarra22@gmail.com>
2022-05-02 16:58:25 +1000

    Async Validator for FormControl

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -17,15 +18,15 @@ export class ReactiveFormComponent implements OnInit {
      * @param of FormControl
      * - default value
      * - Validators
-     * - Async
+     * - Async Validators
      */
     this.signupForm = new FormGroup({
       'userData'  :   new FormGroup({
         'username'  :   new FormControl(null, [Validators.required, this.blockedUser.bind(this)]),
-        'email'     :   new FormControl(null, [Validators.required, Validators.email]),
+        'email'     :   new FormControl(null, [Validators.required, Validators.email], this.blockedEmails),
       }),
       'gender'    :   new FormControl('Male') ,
-      'hobbies'   :   new FormArray([])
+      'hobbies'   :   new FormArray([]),
     });
   }
 
@@ -49,5 +50,20 @@ export class ReactiveFormComponent implements OnInit {
     }
     return null;
   }
+
+  blockedEmails(control: FormControl): Promise<any> | Observable<any>{
+    const promise = new Promise<any>((resolve, reject) =>{
+      setTimeout(() => {
+        if(control.value === 'test@test.com'){
+          resolve({'emailIsBlocked': true});
+        }else{
+          resolve(null);
+        }
+      }, 1500);
+    });
+
+    return promise;
+  }
+
 
 }
